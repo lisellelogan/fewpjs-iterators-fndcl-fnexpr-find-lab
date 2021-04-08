3f06bde13dd8744ce9d32d185b5f66108b958c13
lisellelogan <liselle.logan@yahoo.com>
2021-04-08 11:18:41 -0400

    commit tree 6943bf7715f9197e44552580b1640555af7f9b63

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+//accepts array(of JS obj) as an argument
+//each object has two properties: year and result
+//use find() to test each object to see if the result is "W"
+    //will return the first matching win
+//win should return year when win occurred
+//if no win found, should return undefined
 
+function superbowlWin(record){
+    let result = record.find(record => record.result === "W")
+    return !!result ? result.year : undefined
+}
